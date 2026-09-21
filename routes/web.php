b07fa3c81bcf0f15b49d7966a647feacd033a4f6
raditted <radithya.syabil@gmail.com>
2026-09-21 18:23:21 +0700

    feat: define routes for activities index and show

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActivityController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/activities', [ActivityController::class, 'index'])->name('activities.index');
+Route::get('/activities/{activity}', [ActivityController::class, 'show'])->name('activities.show');
