2ac71be802782b2e6e96247f9cc7d1df5d1f3760
raditted <radithya.syabil@gmail.com>
2026-09-21 18:22:27 +0700

    feat: define fillable attributes and casts for Activity model

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'activity_date', 'category', 'status',];
+    protected function casts(): array
+    {
+        return ['activity_date' => 'date',];
+    }
 }
