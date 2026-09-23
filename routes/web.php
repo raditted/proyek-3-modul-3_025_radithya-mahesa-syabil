971a763a6a4249ceff048040bf60abe480720ae6
raditted <radithya.syabil@gmail.com>
2026-09-23 22:13:55 +0700

    Add welcome route to display the homepage

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\ActivityController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('activities', ActivityController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
